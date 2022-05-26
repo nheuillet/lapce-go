e1ec920a7b7aeb72220bfe54ef5505c1ab6b428a
Noé Heuillet <noe.heuillet@epitech.eu>
2022-05-26 12:47:26 -0400

    fixed match arms on missing gobin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use serde::{Deserialize, Serialize};
-use serde_json::{Value};
+use serde_json::Value;
 
 use lapce_plugin::{register_plugin, start_lsp, LapcePlugin};
 
@@ -12,7 +12,7 @@ struct State {}
 pub struct PluginInfo {
     arch: String,
     os: String,
-    configuration: Configuration
+    configuration: Configuration,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -30,24 +30,32 @@ impl LapcePlugin for State {
         let go_bin_path = match env::var("GOBIN") {
             Ok(var) => var,
             Err(error) => match error {
-                Err(env::VarError::NotPresent) => {
-                    match env::var("GOPATH") {
-                        Ok(var) => format!("{var}/bin"),
-                        Err(error) => {
-                            panic!("Couldn't get GOPATH: {error}")
-                        }
+                env::VarError::NotPresent => match env::var("GOPATH") {
+                    Ok(var) => format!("{var}/bin"),
+                    Err(error) => {
+                        panic!("Couldn't get GOPATH: {error}")
                     }
                 },
-                Err(env::VarError::NotUnicode(val)) => {
+                env::VarError::NotUnicode(val) => {
                     let val = val.to_string_lossy();
                     panic!("GOBIN is not in unicode format: '{val}'")
                 }
-            }
+            },
         };
 
-        let file_name = format!("{}/gopls", go_bin_path.strip_prefix("\"").unwrap().strip_suffix("\"").unwrap());
+        let file_name = format!(
+            "{}/gopls",
+            go_bin_path
+                .strip_prefix("\"")
+                .unwrap()
+                .strip_suffix("\"")
+                .unwrap()
+        );
 
-        start_lsp(&file_name, info.configuration.language_id.as_str(), info.configuration.options)
+        start_lsp(
+            &file_name,
+            info.configuration.language_id.as_str(),
+            info.configuration.options,
+        )
     }
-
 }
